74d5925c5655c52541c30f70f2033a2e805ef036
Braden Griebel <bgriebel@uw.edu>
2025-12-01 13:57:53 -0800

    Created script to perform KO divergence analysis
    Created a script which analyzes the divergence of various subsystems and
    metabolite networks following all single gene knockout events.

---
+++ scripts/04_ko_divergence.py
@@ -0,0 +1,112 @@
+"""
+Script to perform knock-out divergence analysis on the simulation model
+"""
+
+# Setup
+# Imports
+# Standard Library Imports
+from collections import defaultdict
+import pathlib
+import sys
+
+# External Imports
+import cobra  # type: ignore
+import metworkpy
+from metworkpy.metabolites.metabolite_network import (
+    find_metabolite_synthesis_network_reactions,
+    find_metabolite_consuming_network_reactions,
+)
+
+
+# Local Imports
+
+# Path Setup
+if hasattr(sys, "ps1"):
+    # Running in a REPL
+    BASE_PATH = pathlib.Path(".")  # Use current dir as base path
+else:
+    # Running as a file
+    # Use file path to find root
+    BASE_PATH = pathlib.Path(__file__).parent.parent
+MODEL_PATH = BASE_PATH / "models"
+RESULTS_PATH = BASE_PATH / "results" / "ko_divergence"
+
+# Make directories if needed
+RESULTS_PATH.mkdir(parents=True, exist_ok=True)
+
+
+# Script Configuration
+cobra.Configuration().solver = "hybrid"
+DIRECTED = True  # Whether the network should be directed
+SUBSYSTEMS_TO_IGNORE = {"Biomass", "External Exchange Reactions"}
+N_NEIGHBORS = 3  # Number of neighbors to use in divergence estimation
+DIVERGENCE_TYPE = "kl"
+ESSENTIAL_PROPORTION = 0.1
+
+# Read in the simulation model
+sim_model = metworkpy.read_model(MODEL_PATH / "simulation_model.json")
+
+# Find which reactions to not include in the metabolite networks
+rxns_to_ignore_set = set()
+for rxn in sim_model.reactions:
+    if rxn.subsystem in SUBSYSTEMS_TO_IGNORE:
+        rxns_to_ignore_set.add(rxn.id)
+rxns_to_ignore = list(rxns_to_ignore_set)  # Pandas wants a list for drop
+
+# Generate the metabolite synthesis network
+metabolite_synthesis_network = find_metabolite_synthesis_network_reactions(
+    model=sim_model,
+    method="essential",
+    essential_proportion=ESSENTIAL_PROPORTION,
+    progress_bar=False,
+).drop(rxns_to_ignore, axis=0)
+
+# Generate the metabolite consuming network dataframe
+metabolite_consuming_network = find_metabolite_consuming_network_reactions(
+    model=sim_model,
+    reaction_proportion=ESSENTIAL_PROPORTION,
+    check_reverse=True,
+    progress_bar=False,
+).drop(rxns_to_ignore, axis=0)
+
+# Create a dictionary of divergence targets to evaluate
+divergence_targets: dict[str, list[str]] = defaultdict(list)
+
+# Iterate through the reactions to create the divergence targets for subsystems
+for rxn in sim_model.reactions:
+    if rxn.subsystem in SUBSYSTEMS_TO_IGNORE:
+        continue
+    divergence_targets[f"subsystem__{rxn.subsystem}"].append(rxn.id)
+    divergence_targets["subsystem__whole_metabolism"].append(rxn.id)
+
+# Iterate through the metabolite networks to create divergence targets for
+# metabolite networks
+for met, rxn_series in metabolite_synthesis_network.items():
+    divergence_targets[f"metabolite_synthesis__{met}"] = list(
+        rxn_series[rxn_series].index
+    )
+for met, rxn_series in metabolite_consuming_network.items():
+    divergence_targets[f"metabolite_consuming__{met}"] = list(
+        rxn_series[rxn_series].index
+    )
+
+# Filter the divergence targets for any that are of size 0
+# (metabolite A for example has a synthesis network of size 0)
+divergence_targets = {
+    k: v for k, v in divergence_targets.items() if len(v) > 0
+}
+
+# Perform the KO divergence analysis
+ko_divergence_df = metworkpy.divergence.ko_divergence(
+    model=sim_model,
+    genes_to_ko=sorted(sim_model.genes.list_attr("id")),
+    target_networks=divergence_targets,
+    divergence_metric=DIVERGENCE_TYPE,
+    n_neighbors=N_NEIGHBORS,
+).clip(lower=0)  # Divergence should be >0, but
+# this is an estimate, so it can be slightly negative
+# Clipping to correct this somewhat
+
+
+# Save the results of the KO divergence
+ko_divergence_df.to_csv(RESULTS_PATH / "ko_divergence_results.csv", index=True)
