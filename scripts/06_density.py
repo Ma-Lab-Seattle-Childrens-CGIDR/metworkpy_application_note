f5ad3ad401918f120170977e337c1019a59544e9
Braden Griebel <bgriebel@uw.edu>
2025-12-04 15:05:26 -0800

    Added script for target density, udpated pixi environments
    Added a script for finding the target density in the reaction network
    associated with targeted genes. Additionally, added a visualization pixi
    environment including Escher and matplotlib. Additionally, updated the
    pixi environments (lockfiles).

---
+++ scripts/06_density.py
@@ -0,0 +1,73 @@
+"""
+Script to simulate gene target density on the simulation
+model
+"""
+
+# Setup
+# Imports
+# Standard Library Imports
+import pathlib
+import sys
+import tomllib
+
+# External Imports
+import cobra  # type:ignore
+import metworkpy
+from metworkpy.network.density import gene_target_density
+import pandas as pd
+
+# Local Imports
+
+# Path Setup
+if hasattr(sys, "ps1"):
+    # Running in a REPL
+    BASE_PATH = pathlib.Path(".").absolute()  # Use current dir as base path
+else:
+    # Running as a file
+    # Use file path to find root
+    BASE_PATH = pathlib.Path(__file__).parent.parent
+MODEL_PATH = BASE_PATH / "models"
+RESULTS_PATH = BASE_PATH / "results" / "target_density"
+
+# Create directories if needed
+RESULTS_PATH.mkdir(parents=True, exist_ok=True)
+
+# Script Configuration
+# Read in the configuration file
+with open(BASE_PATH / "config.toml", "rb") as f:
+    CONFIG = tomllib.load(f)
+cobra.Configuration().solver = CONFIG["cobra"]["solver"]
+
+# Read in the simulation model
+sim_model = metworkpy.read_model(MODEL_PATH / "simulation_model.json")
+
+# Construct the reaction network from the model
+metabolic_network = metworkpy.create_metabolic_network(
+    model=sim_model,
+    weighted=False,
+    directed=CONFIG["metabolic-network"]["directed"],
+)
+reaction_network = metworkpy.network.bipartite_project(
+    metabolic_network,
+    directed=CONFIG["metabolic-network"]["directed"],
+    node_set=sim_model.reactions.list_attr("id"),
+)
+
+
+# Iterate through the radius list and find the density for each radius
+density_series_list: list[pd.Series] = []
+for radius in CONFIG["target-density"]["radius-list"]:
+    # Create the series of target density
+    target_density_series = gene_target_density(
+        metabolic_network=reaction_network,
+        metabolic_model=sim_model,
+        gene_labels=CONFIG["target-density"]["targeted-genes"],
+        radius=radius,
+    )
+    target_density_series.name = f"Radius: {radius}"
+    density_series_list.append(target_density_series)
+# Concatenate together the different series
+density_df = pd.concat(density_series_list, axis=1)
+
+# Save the results
+density_df.to_csv(RESULTS_PATH / "gene_target_density.csv")
