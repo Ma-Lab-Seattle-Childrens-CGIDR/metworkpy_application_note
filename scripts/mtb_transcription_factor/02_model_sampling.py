44ab790088f03c637af0dce970969b9699566485
Braden Griebel <bgriebel@uw.edu>
2026-01-06 13:39:20 -0800

    feat(mtb tf): Added script for sampling from the generated IMAT models

--- scripts/mtb_transcription_factor/02_model_sampling.py
+++ scripts/mtb_transcription_factor/02_model_sampling.py
@@ -7,6 +7,8 @@ Generate flux samples from iMAT models for all the TFOE strains
 # Standard Library Imports
 import logging
 import pathlib
+import sys
+import tomllib
 
 # External Imports
 import cobra  # type:ignore
@@ -16,39 +18,37 @@ import metworkpy  # type:ignore
 # Local Imports
 
 # Setup Path
-try:
+if hasattr(sys, "ps1"):
+    # Running in a REPL
+    BASE_PATH = pathlib.Path(".")  # Use current dir as base path
+else:
+    # Running as a file
+    # Use file path to find root
     BASE_PATH = pathlib.Path(__file__).parent.parent.parent
-except NameError:
-    BASE_PATH = pathlib.Path(".").absolute()
 DATA_PATH = BASE_PATH / "data"
 CACHE_PATH = BASE_PATH / "cache"
 TFOE_MODELS_PATH = CACHE_PATH / "tf_models"
 FLUX_SAMPLES_PATH = CACHE_PATH / "tf_model_flux_samples"
+LOG_PATH = BASE_PATH / "logs" / "mtb_transcription_factors"
 
 # Make directories if required
 CACHE_PATH.mkdir(parents=True, exist_ok=True)
-TFOE_MODELS_PATH.mkdir(parents=True, exist_ok=True)
 FLUX_SAMPLES_PATH.mkdir(parents=True, exist_ok=True)
 
 # Setup Logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(
-    filename=BASE_PATH
-    / "logs"
-    / "transcription_factors"
-    / "02_model_sampling.log",
+    filename=LOG_PATH / "02_model_sampling.log",
     filemode="w",
     level=logging.INFO,
 )
 
-
-# Run Parameters
-PROCESSES = 12
-THINNING = 500
-NUM_SAMPLES = 1_000
+# Read in the configuration file
+with open(BASE_PATH / "config.toml", "rb") as f:
+    CONFIG = tomllib.load(f)
 
 # Set cobra to use the hybrid solver
-cobra.Configuration().solver = "hybrid"
+cobra.Configuration().solver = CONFIG["cobra"]["solver"]
 
 
 # Generate samples for all the models
@@ -61,16 +61,21 @@ for model_path in (TFOE_MODELS_PATH).glob("*.json"):
         continue  # Already sampled from this
     model = metworkpy.read_model(model_path)
     logger.info("Starting OptGP Sampler")
-    sampler = OptGPSampler(model=model, thinning=THINNING, processes=PROCESSES)
-    samples = sampler.sample(NUM_SAMPLES)
+    sampler = OptGPSampler(
+        model=model,
+        thinning=CONFIG["mtb_tf"]["sampling"]["thinning"],
+        processes=CONFIG["processes"],
+    )
+    samples = sampler.sample(CONFIG["mtb_tf"]["sampling"]["num-samples"])
     valid_samples = samples[sampler.validate(samples) == "v"]  # type:ignore
     logger.info(
-        f"Validated samples, {len(valid_samples) / NUM_SAMPLES:.2%} valid"
+        f"Validated samples, {
+            len(valid_samples)
+            / CONFIG['mtb_tf']['sampling']['num-samples']:.2%} valid"
     )
     logger.info("Saving samples")
     valid_samples.to_parquet(
         flux_sample_out_path,
         index=False,
     )
-
-print("Finished Sampling :-)")
+logger.info("Finished Sampling! ;)")
