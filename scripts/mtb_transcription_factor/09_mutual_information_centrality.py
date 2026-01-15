0274ca2e2be7f685b49c49051d5e4306ed41bcc0
Braden Griebel <bgriebel@uw.edu>
2026-01-15 12:15:43 -0800

    feature(mtb tf): Added script for finding/evaluating mutual information

---
+++ scripts/mtb_transcription_factor/09_mutual_information_centrality.py
@@ -0,0 +1,179 @@
+"""
+Script to analyze the mutual information centrality of the
+iEK1011_v2 model, and the tendency of the transcription factors to
+target more or less central reactions.
+"""
+
+# Setup
+# Imports
+# Standard Library Imports
+import logging
+import pathlib
+import sys
+import tomllib
+
+# External Imports
+import cobra
+import metworkpy
+from metabolic_modeling_utils.false_discovery_control import fdr_with_nan
+import networkx as nx
+import numpy as np
+import pandas as pd
+from scipy import stats
+
+# Local Imports
+
+
+# Path setup
+if hasattr(sys, "ps1"):
+    # Running in a REPL
+    BASE_PATH = pathlib.Path(".")  # Use current dir as base path
+else:
+    # Running as a file
+    # Use file path to find root
+    BASE_PATH = pathlib.Path(__file__).parent.parent.parent
+DATA_PATH = BASE_PATH / "data"
+CACHE_PATH = BASE_PATH / "cache"
+MI_NETWORK_ADJACENCY_PATH = CACHE_PATH / "mtb_mutual_information"
+FLUX_SAMPLES_PATH = CACHE_PATH / "tf_model_flux_path"
+RESULTS_PATH = BASE_PATH / "results" / "mtb_transcription_factors"
+MODELS_PATH = BASE_PATH / "models"
+LOG_PATH = BASE_PATH / "mtb_transcription_factors"
+
+# Setup Logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    filename=LOG_PATH / "08_metabolite_network_target_enrichment.log",
+    filemode="w",
+    level=logging.INFO,
+)
+# Read in the configuration file
+with open(BASE_PATH / "config.toml", "rb") as f:
+    CONFIG = tomllib.load(f)
+
+# Read in the base model for finding mutual information centrality
+cobra.Configuration().solver = CONFIG["cobra"]["solver"]
+BASE_MODEL = metworkpy.read_model(
+    MODELS_PATH / "iEK1011_v2_7H9_ADC_glycerol.json"
+)
+
+# If needed get flux samples for the iEK1011_v2 model
+logger.info("Generating samples if needed")
+wt_flux_sample_path = FLUX_SAMPLES_PATH / "wildtype.parquet"
+if wt_flux_sample_path.exists():
+    logger.info("Reading in previous sampling")
+    flux_sample_df = pd.read_parquet(wt_flux_sample_path)
+else:
+    logger.info("Starting sampling")
+    sampler = cobra.sampling.OptGPSampler(
+        model=BASE_MODEL,
+        thinning=CONFIG["mtb_tf"]["sampling"]["thinning"],
+        processes=CONFIG["processes"],
+    )
+    samples = flux_sample_df = sampler.sample(
+        CONFIG["mtb_tf"]["sampling"]["num-samples"]
+    )
+    valid_samples = samples[sampler.validate(samples) == "v"]  # type: ignore
+    logger.info(
+        f"Validated samples, {
+            len(valid_samples)
+            / CONFIG['mtb_tf']['sampling']['num-samples']:.2%} valid"
+    )
+    valid_samples.to_parquet(wt_flux_sample_path, index=False)
+    flux_sample_df = valid_samples
+
+# Find the mutual information network (if required)
+mi_adj_out_path = MI_NETWORK_ADJACENCY_PATH / "metabolic_mi_adjacency.csv"
+if mi_adj_out_path.exists():
+    mi_adj_df = pd.read_csv(mi_adj_out_path, index_col=0)
+else:
+    mi_adj_df = metworkpy.information.mi_network_adjacency_matrix(
+        samples=flux_sample_df,
+        processes=CONFIG["processes"],
+        progress_bar=False,
+    )
+    mi_adj_df.to_csv(mi_adj_out_path, index=True)
+
+
+# Convert from the mutual information adjacency matrix to a networkx network
+mi_network: nx.Graph[str] = nx.from_pandas_adjacency(
+    mi_adj_df, create_using=nx.Graph
+)
+
+
+# Compute the eigenvector centrality of the networkx graph
+eigenvector_centrality = pd.Series(nx.eigenvector_centrality(mi_network))
+
+# Read in the targets for all of the transcription factors
+# Find the TF targets
+logger.info("Finding the TF targets")
+tf_fc_df = pd.read_excel(
+    DATA_PATH / "mtb_transcription_factors" / "tfoe_targets.xlsx",
+    sheet_name="SupplementaryTableS2",
+    skiprows=list(range(8)) + [9],
+    usecols="A,E:HB",
+    index_col=0,
+)
+tf_pval_df = pd.read_excel(
+    DATA_PATH / "mtb_transcription_factors" / "tfoe_targets.xlsx",
+    sheet_name="SupplementaryTableS2",
+    skiprows=list(range(8)) + [9],
+    usecols="A,HC:OZ",
+    index_col=0,
+)
+tf_pval_df.columns = tf_pval_df.columns.str.replace(".1", "")
+
+tf_target_df = (
+    tf_fc_df.abs() >= CONFIG["mtb_tf"]["target_enrichment"]["target-fc-cutoff"]
+) & (tf_pval_df <= CONFIG["mtb_tf"]["target_enrichment"]["target-pval-cutoff"])
+
+# Get a set of genes in the model
+model_gene_set = set(BASE_MODEL.genes.list_attr("id"))
+
+
+# For each of the transcription factors, evaluate if it has a
+# significantly higher MI centrality than background
+results_df = pd.DataFrame(
+    np.nan,
+    index=tf_target_df.columns,
+    columns=pd.Index(["u1", "u2", "auc", "p-value"]),
+)
+logger.info("Evaluating Eigenvector centrality significance for TF targets")
+for tf, tf_target_series in tf_target_df.items():
+    targeted_genes = set(tf_target_series[tf_target_series]) & model_gene_set
+    non_targeted_genes = model_gene_set - targeted_genes
+    if (
+        len(targeted_genes)
+        < CONFIG["mtb_tf"]["mutual_information"]["min-target-count"]
+    ) or (
+        len(non_targeted_genes)
+        < CONFIG["mtb_tf"]["mutual_information"]["min-target-count"]
+    ):
+        continue
+    targeted_centrality = eigenvector_centrality[list(targeted_genes)]
+    non_targeted_centrality = eigenvector_centrality[list(non_targeted_genes)]
+    # Perform the Mann-Whitney U-test
+    mannu_res = stats.mannwhitneyu(
+        targeted_centrality,
+        non_targeted_centrality,
+        alternative="greater",
+        nan_policy="omit",
+    )
+    # Get u1 and u2
+    u1 = mannu_res.statistic
+    u2 = len(targeted_genes) * len(non_targeted_genes) - u1
+    # Get the AUC
+    auc = u1 / (len(targeted_genes) * len(non_targeted_genes))
+    # Get the p-value
+    pvalue = mannu_res.pvalue
+    # Save results to the dataframe
+    results_df.loc[tf, "u1"] = u1
+    results_df.loc[tf, "u2"] = u2
+    results_df.loc[tf, "auc"] = auc
+    results_df.loc[tf, "p-value"] = pvalue
+# Perform false discovery correction
+results_df["adj p-value"] = fdr_with_nan(results_df["p-value"])
+
+
+# Save the final results
+results_df.to_csv(RESULTS_PATH / "mutual_information_tf_target.csv")
