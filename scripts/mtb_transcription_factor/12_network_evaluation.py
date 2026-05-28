2936579c0da37fda77430e88c355d0b94513d1aa
Braden Griebel <bgriebel@uw.edu>
2026-05-28 10:52:09 -0700

    network-evaluation: Added excluded reactions

--- scripts/mtb_transcription_factor/12_network_evaluation.py
+++ scripts/mtb_transcription_factor/12_network_evaluation.py
@@ -39,6 +39,7 @@ RESULTS_PATH.mkdir(parents=True, exist_ok=True)
 
 def evaluate_network(
     model: cobra.Model,
+    reactions_to_remove: list[str],
     metabolite_counts: pd.Series,
     n: int = 10,
 ) -> pd.Series:
@@ -47,16 +48,19 @@ def evaluate_network(
     then evaluates various properties of the network
     """
     # Construct the network after removing the metabolites
+    logger.info("Constructing the network")
     rxn_network = metworkpy.network.create_reaction_network(
         model=model,
         weighted=False,
         directed=False,
-        nodes_to_remove=list(metabolite_counts.sort(ascending=False).iloc[:n]),
+        nodes_to_remove=reactions_to_remove
+        + list(metabolite_counts.sort_values(ascending=False).iloc[:n]),
     )
     # Create a dict to hold the results
     results_dict = {}
 
     # Calculate the number of components
+    logger.info("Finding the connected components")
     results_dict["component count"] = nx.number_connected_components(
         rxn_network
     )
@@ -67,15 +71,18 @@ def evaluate_network(
     results_dict["largest component size"] = len(rxn_network.nodes)
 
     # Find the algebraic connectivity of the graph
+    logger.info("Finding algebraic connectivity")
     results_dict["algebraic connectivity"] = nx.algebraic_connectivity(
         rxn_network
     )
 
     # Find the small-world coefficients of a graph
+    logger.info("Finding small-world coefficients")
     results_dict["small-world sigma"] = nx.sigma(rxn_network)
     results_dict["small-world omega"] = nx.omega(rxn_network)
 
     # Estimate the average clustering of the graph
+    logger.info("Finding the average clustering")
     results_dict["average clustering estimate"] = (
         nx.approximation.average_clustering(rxn_network)
     )
@@ -89,6 +96,7 @@ def evaluate_network(
             nx.degree_centrality,
         ],
     ):
+        logger.info(f"Finding the {measure} centrality")
         cent_series = pd.Series(centrality_function(rxn_network))
         results_dict[f"{measure} centrality max"] = cent_series.max()
         results_dict[f"{measure} centrality min"] = cent_series.min()
@@ -96,6 +104,7 @@ def evaluate_network(
         results_dict[f"{measure} centrality std"] = cent_series.std()
 
     # Return the results dict as a series
+    logger.info("Returning results")
     res_series = pd.Series(results_dict)
     res_series.name = str(n)
     return res_series
@@ -108,6 +117,8 @@ if __name__ == "__main__":
         filename=LOG_PATH / "12_network_evaluation.log",
         filemode="w",
         level=logging.INFO,
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
 
     # Read in the configuration file
@@ -122,7 +133,20 @@ if __name__ == "__main__":
     BASE_MODEL = metworkpy.read_model(
         MODEL_PATH / "iEK1011_v2_7H9_ADC_glycerol.json"
     )
+
+    logger.info("Finding reactions to remove")
+    subsystems_to_ignore = {
+        "Intracellular demand",
+        "Biomass and maintenance functions",
+        "Extracellular exchange",
+    }
+    reactions_to_remove = set()
+    for rxn in BASE_MODEL.reactions:
+        if rxn.subsystem in subsystems_to_ignore:
+            reactions_to_remove.add(rxn.id)
+
     # Get a count of the number of reactions each metabolite is involved in
+    logger.info("Getting counts of metabolite connectivity")
     stoich_mat = cobra.util.create_stoichiometric_matrix(
         model=BASE_MODEL, array_type="DataFrame"
     )
@@ -133,10 +157,15 @@ if __name__ == "__main__":
 
     res_list = []
     for n in [0, 1, 5, 10, 15, 20, 30, 40, 50]:
+        logger.info(f"Working on n={n}")
         res_list.append(
             evaluate_network(
-                model=BASE_MODEL, metabolite_counts=metabolite_counts, n=n
+                model=BASE_MODEL,
+                reactions_to_remove=reactions_to_remove,
+                metabolite_counts=metabolite_counts,
+                n=n,
             )
         )
+    logger.info("Finished evaluating the networks, saving results")
     res_df = pd.concat(res_list, axis=1).T
     res_df.to_csv(RESULTS_PATH / "network_metabolite_removal_evaluation.csv")
